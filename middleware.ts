bef1864a4117a60cfa7ed229642c7739626c7d26
SyedAliyanHashmi18 <hashmisyedaliyan@gmail.com>
2026-04-19 19:12:57 +0500

    fix: middleware token verification in production

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,30 @@
 import { getToken } from "next-auth/jwt";
-import { NextRequest , NextResponse} from "next/server";
-
+import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(request: NextRequest) {
-    const token = await getToken({ req: request});
-    const { pathname } = request.nextUrl;
+  const token = await getToken({
+    req: request,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
 
-    if( token && (pathname === "/signin" || pathname === "/register") ) {
-        return NextResponse.redirect(new URL("/dashboard", request.url));
+  const { pathname } = request.nextUrl;
 
-    }
-    if( !token && (pathname === "/dashboard" ||
-        pathname.startsWith("/api/projects") ||
-        pathname.startsWith("/api/tasks")
-     ) ){
-        return NextResponse.redirect(new URL("/signin", request.url));
-    }
-    return NextResponse.next();
+  if (token && (pathname === "/signin" || pathname === "/register")) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+  if (
+    !token &&
+    (pathname === "/dashboard" ||
+      pathname.startsWith("/api/projects") ||
+      pathname.startsWith("/api/tasks"))
+  ) {
+    return NextResponse.redirect(new URL("/signin", request.url));
+  }
+
+  return NextResponse.next();
 }
+
 export const config = {
-    matcher: [
-        "/dashboard/:path*", "/api/projects/:path*", "/api/tasks/:path*"],
-}
+  matcher: ["/dashboard/:path*", "/api/projects/:path*", "/api/tasks/:path*"],
+};
